8d25255158e303d4d3107c7fa170b82325172b02
seokjun <tjrwns8024@naver.com>
2019-10-24 23:07:17 +0900

    Signup[로직 구현]+ password_ck

--- Login/app/src/main/java/com/example/login/Signup.java
+++ Login/app/src/main/java/com/example/login/Signup.java
@@ -12,10 +12,13 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class Signup extends AppCompatActivity {
 
+    TextInputLayout signup_edit_password_ck_area;
     TextInputLayout signup_edit_id_area;
+    TextInputLayout signup_edit_user_area;
     TextInputEditText signup_id;
     TextInputEditText signup_password;
     TextInputEditText signup_password_ck;
+    TextInputEditText signup_user;
     Button complete_signup;
 
     @Override
@@ -23,18 +26,24 @@ public class Signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        signup_edit_user_area = findViewById(R.id.signup_edit_user_area);
+        signup_edit_password_ck_area = findViewById(R.id.signup_edit_password_ck_area);
         signup_edit_id_area = findViewById(R.id.signup_edit_id_area);
         signup_id = findViewById(R.id.signup_id);
         signup_password = findViewById(R.id.signup_password);
         signup_password_ck = findViewById(R.id.signup_password_ck);
         complete_signup = findViewById(R.id.complete_signup);
+        signup_user = findViewById(R.id.signup_user);
 
         signup_edit_id_area.setErrorEnabled(true);
+        signup_edit_password_ck_area.setEnabled(true);
+        signup_edit_user_area.setErrorEnabled(true);
 
        complete_signup.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
                emailError(signup_id.getText().toString());
+               passwordCheck(signup_password.getText().toString(), signup_password_ck.getText().toString());
            }
        });
     }
@@ -45,4 +54,12 @@ public class Signup extends AppCompatActivity {
         else
             signup_edit_id_area.setError("이메일 형식대로 입력해주세요");
     }
+    public void passwordCheck(String signup_password, String signup_password_ck){
+        if(signup_password .equals(signup_password_ck)) {
+            signup_edit_password_ck_area.setError("");
+        }
+        else
+            signup_edit_password_ck_area.setError("비밀번호가 일치하지 않아요");
+        }
+
 }
