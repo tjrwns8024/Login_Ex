bda86a4eea73c3e751a05e8d40e3b175a31511c3
seokjun <tjrwns8024@naver.com>
2019-10-24 22:25:58 +0900

    btn_intent

--- Login/app/src/main/java/com/example/login/Signin.java
+++ Login/app/src/main/java/com/example/login/Signin.java
@@ -2,13 +2,37 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Signin extends AppCompatActivity {
+
+    TextView go_signup;
+    TextInputEditText signin_id;
+    TextInputEditText signin_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        go_signup = findViewById(R.id.go_signup);
+        signin_id = findViewById(R.id.signin_id);
+        signin_password = findViewById(R.id.signin_password);
+
+        go_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Signin.this,Signup.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
